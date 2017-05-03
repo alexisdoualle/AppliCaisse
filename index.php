4e2819e63dac2210e51fcbcef3470cd75ad5c862
Alexis <adoualle@gmail.com>
2017-05-03 14:00:52 +0200

    ajout de google analytics

--- index.php
+++ index.php
@@ -12,6 +12,16 @@
     <script src="js/app.js"></script>
     <title>Caisse</title>
   </head>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-98348287-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
   <!-- ng-app initialise l'application angularjs, telle que définie dans app.js-->
   <body ng-app="HLC">
     <!-- Le controleur 'mainCtrl' manipule l'application.
